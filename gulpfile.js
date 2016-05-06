cf29cf29bf66d0dac7a4ee2e2c11794701a71431
nagisa yata <kanbai.8ta.n@gmail.com>
2016-05-06 16:29:41 +0900

    add image

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var sass = require('gulp-sass');
 
 gulp.task('default');
 
@@ -9,4 +10,24 @@ gulp.task('jade', function() {
   gulp.src('*.jade')
     .pipe(jade())
     .pipe(gulp.dest('./'));
+});
+
+gulp.task('sass', function() {
+  gulp.src('./css/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('jade-watch', ['jade'], function() {
+  var watcher = gulp.watch('./*.jade', ['jade']);
+  watcher.on('change', function(event) {
+    console.log('jade: File ' + event.path);
+  });
+});
+
+gulp.task('sass-watch', ['sass'], function() {
+  var watcher = gulp.watch('./css/*.scss', ['sass']);
+  watcher.on('change', function(event) {
+    console.log('sass: File ' + event.path);
+  });
 });
