6dc1652e7040fce775807137a6128ffddbd72304
nagisa yata <kanbai.8ta.n@gmail.com>
2016-05-13 01:24:12 +0900

    Change style and add contents

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(window).on('scroll', function(){
+  $('ul#timeline li').each(function(){
+    //if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+    //  $(this).find('.timeline-point, .timeline-comment').removeClass('is-hidden').addClass('bounce-in');
+    //}
+    console.log(1);
+  });
+});
---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('ul#timeline li').on('scroll', function(){
+  $('ul#timeline li').each(function(){
+    //if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+    //  $(this).find('.timeline-point, .timeline-comment').removeClass('is-hidden').addClass('bounce-in');
+    //}
+    console.log(2);
+  });
+});
