d5571afd71b93e9dc49b5e21fd5f9d50b925c614
nagisa yata <kanbai.8ta.n@gmail.com>
2016-05-13 13:24:54 +0900

    add link of menu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 $(function() {
   // menu
+  function scrollToPage(pageTop) {
+    var speed = 500;
+    var position = pageTop.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  }
+
+  $('li#menu-cover').on('click touchstart', function(){
+    scrollToPage($('#cover'));
+  });
+  $('li#menu-experience').on('click touchstart', function(){
+    scrollToPage($('#experience'));
+  });
+  $('li#menu-skill').on('click touchstart', function(){
+    scrollToPage($('#skill'));
+  });
+  $('li#menu-project').on('click touchstart', function(){
+    scrollToPage($('#project'));
+  });
+  $('li#menu-contact').on('click touchstart', function(){
+    scrollToPage($('#contact'));
+  });
 
   // experience
   var point_num = 11;
