f55b4452c23e26d6b99e830e28f7441e84a9f064
Nagisa Yata <kanbai.8ta.n@gmail.com>
2022-12-13 18:40:10 +0900

    feat: Initialize deno

---
+++ _config.ts
@@ -0,0 +1,15 @@
+import lume from "lume/mod.ts";
+import postcss from "lume/plugins/postcss.ts";
+import terser from "lume/plugins/terser.ts";
+import base_path from "lume/plugins/base_path.ts";
+
+const site = lume({
+    src: "./src",
+    dest: "./output",
+});
+
+site.use(postcss());
+site.use(terser());
+site.use(base_path());
+
+export default site;
--- js/script.js
+++
@@ -1,8 +0,0 @@
-$('ul#timeline li').on('scroll', function(){
-  $('ul#timeline li').each(function(){
-    //if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
-    //  $(this).find('.timeline-point, .timeline-comment').removeClass('is-hidden').addClass('bounce-in');
-    //}
-    console.log(2);
-  });
-});
